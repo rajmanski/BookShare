c177ef595ebf1e04a72601d43a3fda85c0f1cd7c
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-28 12:29:08 +0100

    Fetching books from api is working

--- src/components/shared/HomePage/ApiDownloader.tsx
+++ src/components/shared/HomePage/ApiDownloader.tsx
@@ -1,11 +1,31 @@
+import {useState} from 'react';
+
 export const ApiDownloader = () => {
 
+    const [search, setSearch] = useState('');
+    const [searchedData, setSearchedData] = useState([]);
+
+    const displaySearches = () => {
+        fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}:keyes&key=AIzaSyC3qM70tyz819Oy-fG929Z57AE6QtBBK3A&maxResults=40`)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data.items);
+                setSearchedData(data.items)
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+    
+    
 
     return (
         <div className="container">
             <h1>Search for a book</h1>
-            <input type="text" />
-            <button>Search</button>
+            <input type="text" onChange={(e) => setSearch(e.target.value)}/>
+            <button onClick={displaySearches}>Search</button>
         </div>
     )
 }
