3f14c52388ed19e75cc9230f0e5e063553aeef93
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-28 14:12:34 +0100

    Titles are downloading

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { HomePage } from './components/HomePage/HomePage';
 import { Footer } from './components/Footer/Footer';
 import { RegisterPage } from './components/RegisterPage/RegisterPage';
 
@@ -9,7 +8,6 @@ function App() {
   return (
   
     <div>
-      <HomePage /> 
       <Footer/>
       <RegisterPage />
 
