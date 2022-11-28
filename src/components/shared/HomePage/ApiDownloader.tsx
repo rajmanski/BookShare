e2dc297c68f01d41782d33dd2defb1906e586495
Piotr Rajmanski <prajmanski10@gmail.com>
2022-11-28 11:55:51 +0100

    Books downloader innit

---
+++ src/components/shared/HomePage/ApiDownloader.tsx
@@ -0,0 +1,11 @@
+export const ApiDownloader = () => {
+
+
+    return (
+        <div className="container">
+            <h1>Search for a book</h1>
+            <input type="text" />
+            <button>Search</button>
+        </div>
+    )
+}
