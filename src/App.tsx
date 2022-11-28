8427b421b296a7598e2df24e8379deb6a83b21e2
MiloszKuznia <miloszkuznia@gmail.com>
2022-11-28 13:29:16 +0100

    Rozpoczęcie pracy nad RegiterPanel

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import './App.css';
 import { Footer } from './components/Footer/Footer';
 import { HomePage } from './components/HomePage/HomePage';
+import { RegisterPage } from './components/RegisterPage/RegisterPage';
 
 function App() {
   return (
     <div>
       <HomePage /> 
       <Footer />
+      <RegisterPage />
 
     </div>
   
