54f3baa8c00577bb22b06437c2bfa27356eefaa9
Beata Gonera <beataiwonagonera@gmail.com>
2022-11-29 03:52:59 +0100

    tabs in MyBooksPage added

--- src/components/authorised/MyBooksPage/MyBooksPage.tsx
+++ src/components/authorised/MyBooksPage/MyBooksPage.tsx
@@ -1,9 +1,13 @@
+
 import {Footer} from '../../Footer/Footer'
+import BasicTabs from '../../shared/TabPanel/TabPanel'
 
 export const MyBooksPage = () => {
 
     return(
     <div className="my-books-page-container">
+        <h2 className='books-in-your-library-title'>Books in your library</h2>
+        <BasicTabs/>
 
         <Footer/>
     </div>
