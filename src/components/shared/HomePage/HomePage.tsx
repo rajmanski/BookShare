697df987d2b9d274c2bc80f09e2a2306cf8ac36e
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-02 07:27:57 +0100

    Modal added

--- src/components/shared/HomePage/HomePage.tsx
+++ src/components/shared/HomePage/HomePage.tsx
@@ -1,13 +1,29 @@
 import { Button, TextField } from "@mui/material";
 import { useState } from "react";
 import { Footer } from "../../Footer/Footer";
-import { NavBar } from '../NavBar/NavBar';
+import { NavBar } from "../NavBar/NavBar";
 import FavoriteIcon from "@mui/icons-material/Favorite";
+import Modal from '@mui/material/Modal'
+import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
 import "./HomePage.style.css";
 
 export const HomePage = () => {
   const [search, setSearch] = useState("");
   const [searchedData, setSearchedData] = useState([]);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style={
+    width: '400px',
+    height: '400px',
+    position: 'absolute',
+    left: 'calc(50% - 150px)',
+    top: '30%', 
+    backgroundColor: 'white',
+    
+}
 
   const displaySearches = () => {
     fetch(
@@ -27,12 +43,12 @@ export const HomePage = () => {
 
   return (
     <div className="home-page-container">
-      <NavBar/>
+      <NavBar />
       <div className="search-area">
         <div className="search">
           <h1>Share your books and be eco-friendly.</h1>
           <p>
-            Not a fan of ebooks?Not enough spacein your bookshelf to buy more
+            Not a fan of ebooks? Not enough spacein your bookshelf to buy more
             books? Want to save some money and get access to the newest
             booksellers?<br></br>
             Become a member of 30.000+ Bookshare community!
@@ -60,7 +76,11 @@ export const HomePage = () => {
         </div>
         <div className="images">
           <div className="img-wrapper">
-            <img className="first-book" src="jo.jpg" alt="Jo Nesbo book cover" />
+            <img
+              className="first-book"
+              src="jo.jpg"
+              alt="Jo Nesbo book cover"
+            />
             <img
               className="second-book"
               src="king.jpg"
@@ -75,9 +95,24 @@ export const HomePage = () => {
         </div>
       </div>
       <div className="book-area">
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Book Info
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit aperiam fugiat illum, iste facere nesciunt nihil officiis earum ratione itaque, suscipit corporis inventore? Inventore maxime sit eum tenetur minus quidem adipisci dicta dolores! Earum, delectus. Possimus distinctio quis velit, sapiente, laudantium sequi amet, incidunt minima eum necessitatibus eius perspiciatis optio!
+            </Typography>
+          </Box>
+        </Modal>
         <h1>New in Bookshare</h1>
         <div className="books-card-area">
-          <div className="card-on-homepage">
+          <div className="card-on-homepage" onClick={handleOpen}>
             <div className="img-card-wrapper">
               <img src="shantaram.jpg" alt="Shantaram" />
             </div>
@@ -87,9 +122,13 @@ export const HomePage = () => {
             </div>
             <div className="author">Gregory D. Roberts</div>
             <div className="buttons">
-              <Button variant="text" size="small" sx={{color: 'blue'}}>BORROW</Button>
-              <Button variant="text" size="small" sx={{color: 'blue'}}>DETAILS</Button>
-              <FavoriteIcon sx={{color: 'gray'}}/>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                BORROW
+              </Button>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                DETAILS
+              </Button>
+              <FavoriteIcon sx={{ color: "gray" }} />
             </div>
           </div>
           <div className="card-on-homepage">
@@ -102,9 +141,13 @@ export const HomePage = () => {
             </div>
             <div className="author">Gregory D. Roberts</div>
             <div className="buttons">
-              <Button variant="text" size="small" sx={{color: 'blue'}}>BORROW</Button>
-              <Button variant="text" size="small" sx={{color: 'blue'}}>DETAILS</Button>
-              <FavoriteIcon sx={{color: 'gray'}}/>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                BORROW
+              </Button>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                DETAILS
+              </Button>
+              <FavoriteIcon sx={{ color: "gray" }} />
             </div>
           </div>
           <div className="card-on-homepage">
@@ -117,9 +160,13 @@ export const HomePage = () => {
             </div>
             <div className="author">Gregory D. Roberts</div>
             <div className="buttons">
-              <Button variant="text" size="small" sx={{color: 'blue'}}>BORROW</Button>
-              <Button variant="text" size="small" sx={{color: 'blue'}}>DETAILS</Button>
-              <FavoriteIcon sx={{color: 'gray'}}/>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                BORROW
+              </Button>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                DETAILS
+              </Button>
+              <FavoriteIcon sx={{ color: "gray" }} />
             </div>
           </div>
           <div className="card-on-homepage">
@@ -132,9 +179,13 @@ export const HomePage = () => {
             </div>
             <div className="author">Gregory D. Roberts</div>
             <div className="buttons">
-              <Button variant="text" size="small" sx={{color: 'blue'}}>BORROW</Button>
-              <Button variant="text" size="small" sx={{color: 'blue'}}>DETAILS</Button>
-              <FavoriteIcon sx={{color: 'gray'}}/>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                BORROW
+              </Button>
+              <Button variant="text" size="small" sx={{ color: "blue" }}>
+                DETAILS
+              </Button>
+              <FavoriteIcon sx={{ color: "gray" }} />
             </div>
           </div>
         </div>
