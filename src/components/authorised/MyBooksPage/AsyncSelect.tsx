f245808cf50af47bea77d4d41927907d1c4cdaae
Beata Gonera <beataiwonagonera@gmail.com>
2022-12-02 10:54:19 +0100

    ApiDownloaderTitle fixed

--- src/components/authorised/MyBooksPage/AsyncSelect.tsx
+++ src/components/authorised/MyBooksPage/AsyncSelect.tsx
@@ -1,17 +1,20 @@
+import { VolumeDown } from '@mui/icons-material';
 import { useState } from 'react';
 import AsyncSelect from 'react-select/async';
 
 
 export const MyAsyncSelect = () => {
 
+    const [searchedBooks, setSearchedBooks] = useState([])
+
     const fetchBooks = () => {
         fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:harry&printType=books&key=AIzaSyC3qM70tyz819Oy-fG929Z57AE6QtBBK3A&maxResults=10`)
         .then((response) => {
-            response.json()
+           return response.json()
         })
         .then((data) => {
             console.log(data)
-            return (data);
+            setSearchedBooks(data.items[0].volumeInfo.title)
         })
         .catch((error) => {
             console.log(error);
--- src/components/authorised/MyBooksPage/MyBooksPage.tsx
+++ src/components/authorised/MyBooksPage/MyBooksPage.tsx
@@ -37,7 +37,7 @@ export const MyBooksPage = () => {
     </div>
 
     <BooksModal/>
-    {/* <MyAsyncSelect/> */}
+    <MyAsyncSelect/>
     <Footer/>
     </>
     )
