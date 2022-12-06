10bfec4f57d3f82dd36d8cae56eb244f60da9110
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-06 11:21:02 +0100

    Map modal is displaying

--- src/components/shared/Borrow/Borrow.tsx
+++ src/components/shared/Borrow/Borrow.tsx
@@ -1,12 +1,39 @@
-import { Avatar, Button, Rating } from "@mui/material";
+import { Avatar, Box, Button, Modal, Rating, Typography } from "@mui/material";
 import { Footer } from "../../Footer/Footer";
 import { NavBar } from "../NavBar/NavBar";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./Borrow.style.css";
 import { pink } from "@mui/material/colors";
+import { useState } from "react";
 
 export const Borrow = () => {
+
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const style = {
+    width: "800px",
+    height: "600px",
+    position: "absolute",
+    left: "calc(50% - 400px)",
+    top: "15%",
+    backgroundColor: "white",
+    margin: "20px",
+    padding: "20px",
+    border: "none",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    gap: "20px",
+    borderColor: "white",
+    borderRadius: "6px",
+    filter: "drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25))",
+    outline: '0',
+  };
+
   return (
     <div className="borrow-page-container">
       <NavBar />
@@ -33,9 +60,67 @@ export const Borrow = () => {
                 <div className="borrowed-book-card-buttons">
                   <Button sx={{ color: "#1976D2" }}>Return</Button>
                   <Button sx={{ color: "#1976D2" }}>Prolong</Button>
-                  <LocationOnOutlinedIcon />
+                  <LocationOnOutlinedIcon  onClick={handleOpen} sx={{cursor: 'pointer'}}/>
                 </div>
               </div>
+              <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className="modal-data">
+              <h5>Owner: Piotrek</h5>
+              <h5>Avaliable from: 4 Dec 2022</h5>
+              <h5>Pick-up spot: ul. Jana Pawła II 28/32</h5>
+            </div>
+            <div className="title-and-author">
+              <Typography id="modal-modal-title" variant="h4" component="h2">
+                Shantaram
+              </Typography>
+              <h5>Gregory D. Roberts</h5>
+            </div>
+            <Typography
+              id="modal-modal-description"
+              sx={{ mt: 2, color: "gray" }}
+            >
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit
+              aperiam fugiat illum, iste facere nesciunt nihil officiis earum
+              ratione itaque, suscipit corporis inventore? Inventore maxime sit
+              eum tenetur minus quidem adipisci dicta dolores! Earum, delectus.
+              Possimus distinctio quis velit, sapiente, laudantium sequi amet,
+              incidunt minima eum necessitatibus eius perspiciatis optio! Lorem
+              ipsum dolor sit amet, consectetur adipisicing elit. A aliquid sit
+              obcaecati commodi, repudiandae sunt animi assumenda, placeat
+              tempore dolores magni quia quisquam minus rerum! Ipsam, molestias.
+              Omnis et nihil eos, vitae soluta nam deleniti saepe repellendus
+              quia cum dolore amet tenetur delectus, dolorum inventore error
+              totam eius, placeat ipsa!
+            </Typography>
+            <div className="raiting-and-button">
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-around",
+                  width: "100%",
+                  gap: "550px",
+                }}
+              >
+                <Rating name="simple-controlled" value={2} />
+                <Button
+                  sx={{
+                    bgcolor: "#18a86e",
+                    "&:hover": { backgroundColor: "#405d27" },
+                  }}
+                  variant="contained"
+                >
+                  Borrow
+                </Button>
+              </Box>
+            </div>
+          </Box>
+        </Modal>
             </div>
             <div className="borrowed-book-card">
               <div className="top-section">
@@ -56,7 +141,7 @@ export const Borrow = () => {
                 <div className="borrowed-book-card-buttons">
                   <Button sx={{ color: "#1976D2" }}>Return</Button>
                   <Button sx={{ color: "#1976D2" }}>Prolong</Button>
-                  <LocationOnOutlinedIcon />
+                  <LocationOnOutlinedIcon/>
                 </div>
               </div>
             </div>
