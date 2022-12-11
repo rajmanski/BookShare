2a0eebd1abea987fbe5b5e251369c75432f906e0
Beata Gonera <beataiwonagonera@gmail.com>
2022-12-11 21:11:59 +0100

    adding books to shared in tabs

--- src/components/shared/TabPanel/TabPanel.tsx
+++ src/components/shared/TabPanel/TabPanel.tsx
@@ -4,15 +4,12 @@ import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import { useTheme } from '@mui/material/styles';
 import { CardMyBooksPage } from '../../authorised/CardMyBooksPage/CardMyBooksPage';
 import { SxProps, Theme } from '@mui/material/styles';
-import cover from '../../../images/Book2.jpeg'
 import { getDocs, collection} from 'firebase/firestore';
 import { db } from '../../../firebase'
 import { getAuth, onAuthStateChanged } from 'firebase/auth'
 import '../TabPanel/TabPanel.style.css'
-import { Url } from 'url';
 
 
 interface TabPanelProps {
@@ -58,50 +55,57 @@ function a11yProps(index: number) {
 }
 
 interface BasicTabsInterfaceProps{
-  newBook: string
+  newBook: string;
 }
 
 export const BasicTabs:FC<BasicTabsInterfaceProps> = ({newBook}) => {
   const [value, setValue] = useState(0);
-  const theme = useTheme();
   const [email, setEmail] = useState<string | null>(null);
 
   const auth = getAuth()
-
   
-  
-  const [privateBooksIDs, setPrivateBooksIDs] = useState<string[]>([])  
+  const [privateBooksIDs, setPrivateBooksIDs] = useState<string[]>([])
+  const [sharedBooksIDs, setSharedBooksIDs] = useState<string[]>([])  
   const [privateBooksDetails, setPrivateBooksDetails] = useState<privateBooksInterface[] | null>(null) 
-
-
-
-  useEffect(()=> {
-    let privateBooksIDsArr: string[] = [];
-
-    onAuthStateChanged(auth, (user) => {
-      if (user) {
-        const email = user.email;
-        setEmail(email)
-        getDocs(collection( db, `users/${email}/ownedBooks`))
-      .then((querySnapshot) => {
-          querySnapshot.docs.forEach((doc) => {
-              let privateBookID = doc.id
-              privateBooksIDsArr.push(privateBookID)
-          })
-          setPrivateBooksIDs(privateBooksIDsArr)
-          // console.log(privateBooksIDs)
-      })  
-      } else {
-      console.log('user is signed out')
-      }
-    });   
-},[newBook])
-
-let booksArr:privateBooksInterface[] = []
+  const [sharedBooksDetails, setSharedBooksDetails] = useState<privateBooksInterface[] | null>(null) 
+  const [sharedBook, setSharedBook] = useState('')
 
 
 useEffect(() => {
-  var urls = privateBooksIDs?.map ((privateBookID) =>`https://www.googleapis.com/books/v1/volumes/${privateBookID}`)
+  let privateBooksFromDBArr: object[] = []
+  let privateBooksIDsArr: string[] = [];
+  let sharedBooksIDsArr: string[] = [];
+
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      const email = user.email;
+      setEmail(email)
+      getDocs(collection( db, `users/${email}/ownedBooks`))
+    .then((querySnapshot) => {
+        querySnapshot.docs.forEach((doc) => {
+            let bookFromDB = doc.data()
+            let bookID = doc.id
+            if(bookFromDB.isShared == false){
+              privateBooksIDsArr.push(bookID)
+            }else{
+              sharedBooksIDsArr.push(bookID)
+            }
+        })
+        setPrivateBooksIDs(privateBooksIDsArr)
+        setSharedBooksIDs(sharedBooksIDsArr)
+
+    })  
+    } else {
+    console.log('user is signed out')
+    }
+  });   
+},[newBook, sharedBook])
+
+let privateBooksArr:privateBooksInterface[] = []
+
+useEffect(() => {
+  let urls = privateBooksIDs?.map ((privateBookID) =>`https://www.googleapis.com/books/v1/volumes/${privateBookID}`)
   const toRequest = url => fetch(url)
     .then((response) => {
       return response.json()
@@ -112,7 +116,7 @@ useEffect(() => {
       console.log(data)
       data.forEach((book) => {
         if(book.volumeInfo.imageLinks){
-          booksArr.push({
+          privateBooksArr.push({
             volumeID: book.id, 
             title: book.volumeInfo.title,
             subTitle: book.volumeInfo.subtitle,
@@ -120,7 +124,7 @@ useEffect(() => {
             cover: book.volumeInfo.imageLinks.thumbnail
           })
         }else{
-          booksArr.push({
+          privateBooksArr.push({
             volumeID: book.id, 
             title: book.volumeInfo.title,
             subTitle: book.volumeInfo.subtitle,
@@ -129,36 +133,48 @@ useEffect(() => {
         }
        
     })
-    setPrivateBooksDetails(booksArr)
+    setPrivateBooksDetails(privateBooksArr)
     })
+}, [privateBooksIDs])
 
-  // const getBookDetails = () => {
-  //   privateBooksIDs?.forEach((privateBookID) => {
-  //     fetch(`https://www.googleapis.com/books/v1/volumes/${privateBookID}`)
-  //     .then((response) => {
-  //       return response.json()
-  //     })
-  //     .then((data) => {
-  //       booksArr.push({
-  //         volumeID: data.id, 
-  //         title: data.volumeInfo.title,
-  //         subTitle: data.volumeInfo.subtitle,
-  //         authors: data.volumeInfo.authors
-  //         // cover: item.volumeInfo.imageLinks.thumbnail
-  //       })
-  //       setPrivateBooksDetails(booksArr)
-  //       console.log(booksArr)
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     })
-  //   })
-  // }
-  // getBookDetails()
+
+let sharedBooksArr:privateBooksInterface[] = []
+
+
+useEffect(() => {
+  let urls = sharedBooksIDs?.map ((sharedBookID) =>`https://www.googleapis.com/books/v1/volumes/${sharedBookID}`)
+  const toRequest = url => fetch(url)
+    .then((response) => {
+      return response.json()
+    })
+  Promise.all(urls.map(toRequest))
+ 
+    .then((data) => {
+      console.log(data)
+      data.forEach((book) => {
+        if(book.volumeInfo.imageLinks){
+          sharedBooksArr.push({
+            volumeID: book.id, 
+            title: book.volumeInfo.title,
+            subTitle: book.volumeInfo.subtitle,
+            authors: book.volumeInfo.authors,
+            cover: book.volumeInfo.imageLinks.thumbnail
+          })
+        }else{
+          sharedBooksArr.push({
+            volumeID: book.id, 
+            title: book.volumeInfo.title,
+            subTitle: book.volumeInfo.subtitle,
+            authors: book.volumeInfo.authors
+          })
+        }
+       
+    })
+    setSharedBooksDetails(sharedBooksArr)
+    })
 }, [privateBooksIDs])
 
   
-
   const handleChange = (event: React.SyntheticEvent, newValue: number) => {
     setValue(newValue);
   };
@@ -181,13 +197,18 @@ useEffect(() => {
           <div className='private-books-container'>
             {!privateBooksIDs && <div className='add-books-div'>Add books to your library to see them here</div>}
             {privateBooksDetails && privateBooksDetails.map((book) => (
-              <CardMyBooksPage bookCover={book.cover} bookTitle={book.title} bookAuthor={book.authors[0]}/>
+              <CardMyBooksPage volumeID={book.volumeID} bookCover={book.cover} bookTitle={book.title} bookAuthor={book.authors[0]} setSharedBook={setSharedBook}/>
             ))}
           </div>
 
       </TabPanel>
       <TabPanel value={value} index={1}>
-        Shared books collection
+        <div className='private-books-container'>
+          {!sharedBooksIDs && <div className='add-books-div'>Share books from your private library to see them here</div>}
+          {sharedBooksDetails && sharedBooksDetails.map((book) => (
+              <CardMyBooksPage volumeID={book.volumeID} bookCover={book.cover} bookTitle={book.title} bookAuthor={book.authors[0]} setSharedBook={setSharedBook}/>
+          ))}
+        </div>
       </TabPanel>
     </Box>
   );
