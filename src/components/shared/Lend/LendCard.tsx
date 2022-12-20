12e3ecd18c89785a095c6343d4d6b744fe2527a1
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-20 15:33:08 +0100

    Styling Lend component

--- src/components/shared/Lend/LendCard.tsx
+++ src/components/shared/Lend/LendCard.tsx
@@ -1,5 +1,42 @@
+import { Button } from '@mui/material'
+import './LendCard.style.css'
+
 export const LendCard = ({booksInfo, dbData}) => {
+
+  let image = booksInfo.imageLinks.thumbnail;
+
+  if (image === undefined) {
+    image = 'nocover.png'
+  }
+
+  if (booksInfo.title.length > 45) {
+    booksInfo.title = booksInfo.title.slice(0, 45) + "...";
+  }
+
+//   if (booksInfo.volumeInfo?.description === undefined) {
+//     booksInfo.volumeInfo.description = 'Description is not avaliable';
+//   }
+
     return (
-        <p>card</p>
+        <div className="card-on-lend">
+             <div className="img-card-wrapper">
+          <img src={image} alt={booksInfo.title}/>
+
+        </div>
+        <div className="title-and-area">
+          <h3>{booksInfo.title}</h3>
+          <h4>{dbData.city}</h4>
+        </div>
+        <div className="author">{booksInfo.authors[0]}</div>
+        <div className="buttons">
+          <Button variant="text" size="small" sx={{ color: "blue" }}>
+            BORROW
+          </Button>
+          <Button variant="text" size="small" sx={{ color: "blue" }}>
+            DETAILS
+          </Button>
+        </div>
+        </div>
+        
     )
 }
