9738fe7e2778f45784e85cbda986fdb500a906c8
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-21 15:16:31 +0100

    Code refactor

--- src/components/shared/Borrow/AsyncSelectBorrow.tsx
+++ src/components/shared/Borrow/AsyncSelectBorrow.tsx
@@ -29,7 +29,6 @@ export const AsyncSelectBorrow: FC<AsyncAutocompleteBooksInterface> = ({
   setFoundBook,
 }) => {
   const [search, setSearch] = useState("");
-  const [booksInfo, setBooksInfo] = useState();
   const [searchedBooks, setSearchedBooks] = useState<any>([]);
 
   const [titleChosen, setTitleChosen] = useState("");
@@ -44,47 +43,31 @@ export const AsyncSelectBorrow: FC<AsyncAutocompleteBooksInterface> = ({
     },
   ];
 
-  const [booksToBorrow, setBooksToBorrow] = useState({});
-
   const getBooksIds = async () => {
     const emails: string[] = [];
     const booksList: string[] = [];
-    const volumeEmailObject: any = {}
-    const information: any = [];
     const querySnapshot = await getDocs(collection(db, `users`));
     querySnapshot.forEach((doc) => {
       emails.push(doc.id);
     });
-    // setEmails(emailList);
     for (let i = 0; i < emails.length; i++) {
       const querySnapshot2 = await getDocs(
-        // collection(db, `users/${emails[i]}/ownedBooks`)
         query(collection(db, `users/${emails[i]}/ownedBooks`), where('isShared', '==', true))
       );
       querySnapshot2.forEach((doc) => {
         booksList.push(doc.data().volumeID);
       });
-      // volumeEmailObject[emails[i]] = booksList;
     }
 
     const getApiData = async () => {
       const responseList: any = [];
       for (let i = 0; i < booksList.length; i++) {
         const response = await fetch(
-
           `https://www.googleapis.com/books/v1/volumes/${booksList[i]}`
-
-          // `https://www.googleapis.com/books/v1/volumes/${booksList[i]}?:keyes&key=AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8&maxResults=10`
-          //key1: AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8
-          //key2 : AIzaSyC3qM70tyz819Oy-fG929Z57AE6QtBBK3A
-          //key3: AIzaSyDxOP0RY7OpFfbHIZHByQJDGV2Hfh9rV6o
         );
         const data = await response.json();
-        // data.volumeInfo['info'] = 'tekst';
         responseList.push(data);
       }
-      console.log(responseList);
-      
       setSearchedBooks(responseList.sort());
     };
     getApiData();
@@ -93,6 +76,8 @@ export const AsyncSelectBorrow: FC<AsyncAutocompleteBooksInterface> = ({
   useEffect(() => {
     getBooksIds()
   }, []);
+
+
 
   return (
     <Autocomplete
