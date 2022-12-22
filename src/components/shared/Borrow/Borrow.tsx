139c40b795a45327c1ac9cd00f2076ff2ea87d54
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-22 15:32:42 +0100

    There are more books on the borrow component to button clikc -> show more books

--- src/components/shared/Borrow/Borrow.tsx
+++ src/components/shared/Borrow/Borrow.tsx
@@ -25,6 +25,7 @@ export const Borrow = () => {
   const [volumeIds, setVolumeIds] = useState<string[]>([]);
   const [displayBook, setDisplayBook] = useState(true)
   const [sharedBook, setSharedBook] = useState();
+  const [showMore, setShowMore] = useState(false);
   const handleClose = () => setOpen(false);
 
   const user = auth.currentUser;
@@ -74,52 +75,52 @@ export const Borrow = () => {
     }
     getApiData();
   }
-  const getSharedBooks = async () => {
-    const emails: string[] = [];
-    const booksList: string[] = [];
-    const volumeEmailObject: any = {}
-    const information: any = [];
-    const querySnapshot = await getDocs(collection(db, `users`));
-    querySnapshot.forEach((doc) => {
-      emails.push(doc.id);
-    });
-    // setEmails(emailList);
-    for (let i = 0; i < 8; i++) {
-      const querySnapshot2 = await getDocs(
-        // collection(db, `users/${emails[i]}/ownedBooks`)
-        query(collection(db, `users/${emails[i]}/ownedBooks`), where('isShared', '==', true))
-      );
-      querySnapshot2.forEach((doc) => {
-        information.push(doc.data());
-        booksList.push(doc.data().volumeID);
-      });
-    }
+  // const getSharedBooks = async () => {
+  //   const emails: string[] = [];
+  //   const booksList: string[] = [];
+  //   const volumeEmailObject: any = {}
+  //   const information: any = [];
+  //   const querySnapshot = await getDocs(collection(db, `users`));
+  //   querySnapshot.forEach((doc) => {
+  //     emails.push(doc.id);
+  //   });
+  //   // setEmails(emailList);
+  //   for (let i = 0; i < 8; i++) {
+  //     const querySnapshot2 = await getDocs(
+  //       // collection(db, `users/${emails[i]}/ownedBooks`)
+  //       query(collection(db, `users/${emails[i]}/ownedBooks`), where('isShared', '==', true))
+  //     );
+  //     querySnapshot2.forEach((doc) => {
+  //       information.push(doc.data());
+  //       booksList.push(doc.data().volumeID);
+  //     });
+  //   }
     
     
-    const getApiData = async () => {
-      const responseList: any = [];
-      for (let i = 0; i < 8; i++) {
-        const response = await fetch(
+  //   const getApiData = async () => {
+  //     const responseList: any = [];
+  //     for (let i = 0; i < 8; i++) {
+  //       const response = await fetch(
 
-          `https://www.googleapis.com/books/v1/volumes/${booksList[i]}`
+  //         `https://www.googleapis.com/books/v1/volumes/${booksList[i]}`
 
-          // `https://www.googleapis.com/books/v1/volumes/${booksList[i]}?:keyes&key=AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8&maxResults=10`
-          //key1: AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8
-          //key2 : AIzaSyC3qM70tyz819Oy-fG929Z57AE6QtBBK3A
-          //key3: AIzaSyDxOP0RY7OpFfbHIZHByQJDGV2Hfh9rV6o
-        );
-        const data = await response.json();
-        // data.volumeInfo['info'] = 'tekst';
-        responseList.push(data.volumeInfo);
-      }
-      setSharedBook(responseList);
-    };
-    getApiData();
-  };
+  //         // `https://www.googleapis.com/books/v1/volumes/${booksList[i]}?:keyes&key=AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8&maxResults=10`
+  //         //key1: AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8
+  //         //key2 : AIzaSyC3qM70tyz819Oy-fG929Z57AE6QtBBK3A
+  //         //key3: AIzaSyDxOP0RY7OpFfbHIZHByQJDGV2Hfh9rV6o
+  //       );
+  //       const data = await response.json();
+  //       // data.volumeInfo['info'] = 'tekst';
+  //       responseList.push(data.volumeInfo);
+  //     }
+  //     setSharedBook(responseList);
+  //   };
+  //   getApiData();
+  // };
 
   useEffect(() => {
     getBorrowedBooks();
-    getSharedBooks();
+    // getSharedBooks();
   }, [displayBook])
 
   
@@ -282,9 +283,137 @@ export const Borrow = () => {
                 </div>
               </div>
             </div>
+            {showMore && (
+              <>
+                <div className="first-card-container">
+              <div className="first-card-img">
+                <img src="carriesoto is back.jpg" alt="Carie Soto is back book cover" />
+              </div>
+              <div className="first-card-comment">
+                <div className="top">
+                  <div className="rating">
+                    <Rating name="read-only" value={4} readOnly />
+                  </div>
+                  <div className="text">
+                    “The only other TJR novel I’ve read was one that thoroughly stunned me—DAISY JONES AND THE SIX. Reid captured the time period, the rock scene, and the characters with so much heart and authenticity that I was open and ready to read another of her works. But tennis? It wasn’t until I scanned some reviews that specifically stated that you don’t need to understand tennis to enjoy this book.”{" "}
+                  </div>
+                  <div className="user-info">
+                    <div className="avatar">
+                      <Avatar sx={{ bgcolor: green[500] }}>BH</Avatar>
+                    </div>
+                    <div className="username">Beatsy Van Horn</div>
+                  </div>
+                </div>
+                <div className="card-bottom-btn-section">
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    Borrow
+                  </Button>
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    DETAILS
+                  </Button>
+                  <FavoriteIcon sx={{ color: "gray" }} />
+                </div>
+              </div>
+            </div>
+            <div className="second-card-container">
+              <div className="second-card-img">
+                <img src="novel.jpg" alt="Novel book cover" />
+              </div>
+              <div className="second-card-comment">
+                <div className="top">
+                  <div className="rating">
+                    <Rating name="read-only" value={2} readOnly />
+                  </div>
+                  <div className="text">
+                  Demon Copperhead by Barbara Kingsolver is a long novel with strong messages, as are many of her books. Told from the perspective, and in the voice of, a boy named Demon Copperhead, the reader accompanies him from his birth to a single mother without a penny to her name.
+.{" "}
+                  </div>
+                  <div className="user-info">
+                    <div className="avatar">
+                      <Avatar sx={{ bgcolor: pink[500] }}>BB</Avatar>
+                    </div>
+                    <div className="username">Bonnie Brodie</div>
+                  </div>
+                </div>
+                <div className="card-bottom-btn-section">
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    Borrow
+                  </Button>
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    DETAILS
+                  </Button>
+                  <FavoriteIcon sx={{ color: "gray" }} />
+                </div>
+              </div>
+            </div>
+            <div className="third-card-container">
+              <div className="third-card-comment">
+                <div className="top">
+                  <div className="rating">
+                    <Rating name="read-only" value={5} readOnly />
+                  </div>
+                  <div className="text">
+                  Jonathan Freedland's book, "The Escape Artist," presents the story of Walter Rosenberg (who later changed his name to Rudolf Vrba), a teenaged Slovakian Jewish lad, who suffered the indignities inflicted by the Nazi governments that took over Hitler's annexed areas. At first. 
+                  </div>
+                  <div className="user-info">
+                    <div className="avatar">
+                      <Avatar sx={{ bgcolor: yellow[500] }}>MM</Avatar>
+                    </div>
+                    <div className="username">Mike Mismuke</div>
+                  </div>
+                </div>
+                <div className="card-bottom-btn-section">
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    Borrow
+                  </Button>
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    DETAILS
+                  </Button>
+                  <FavoriteIcon sx={{ color: "gray" }} />
+                </div>
+              </div>
+              <div className="third-card-img">
+                <img src="escape.jpg" alt="The Escape Artist book cover"/>
+              </div>
+            </div>
+            <div className="forth-card-container">
+              <div className="forth-card-img">
+                <img src="tomorrow.jpg" alt="Tomorrow book cover" />
+              </div>
+              <div className="forth-card-comment">
+                <div className="top">
+                  <div className="rating">
+                    <Rating name="read-only" value={5} readOnly />
+                  </div>
+                  <div className="text">
+                  I loved everything about this book…
+It’s adorable, sweet, sad, theatrical, character-collaborative-driven-in-spirit, artful, smart, emotional, intellectually rigorous, perceptive, and wonderful…
+I could name a dozen more vertiginously exciting . . . vibrant words
+to reflect the deep satisfying experience this novel is.
+                  </div>
+                  <div className="user-info">
+                    <div className="avatar">
+                      <Avatar sx={{ bgcolor: red[500] }}>MM</Avatar>
+                    </div>
+                    <div className="username">Mindi Mitchell</div>
+                  </div>
+                </div>
+                <div className="card-bottom-btn-section">
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    Borrow
+                  </Button>
+                  <Button variant="text" sx={{ color: "#1976D2" }}>
+                    DETAILS
+                  </Button>
+                  <FavoriteIcon sx={{ color: "gray" }} />
+                </div>
+              </div>
+            </div>
+              </>
+            )}
           </div>
           <div className="borrow-book-button">
-            <Button variant="outlined" sx={{ width: "207px", height: "42px" }}>
+            <Button variant="outlined" sx={{ width: "207px", height: "42px" }} onClick={() => setShowMore(true)}>
               SHOW MORE BOOKS
             </Button>
           </div>
