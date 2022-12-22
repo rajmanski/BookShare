faf9ff2d8a9d361972ed904ad28db3332503a0a7
Piotr Rajmanski <prajmanski10@gmail.com>
2022-12-22 14:40:51 +0100

    Avatars and another people added on borrow page

--- src/components/shared/Borrow/Borrow.tsx
+++ src/components/shared/Borrow/Borrow.tsx
@@ -4,7 +4,7 @@ import { NavBar } from "../NavBar/NavBar";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import "./Borrow.style.css";
-import { pink } from "@mui/material/colors";
+import { green, pink, red, yellow } from "@mui/material/colors";
 import { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
@@ -13,7 +13,7 @@ import {Icon} from 'leaflet'
 import { BorrowModal } from '../../shared/Borrow/BorrowModal'
 import { PersistentDrawerLeft } from "../NavBar/Drawer";
 import { BorrowedBookCard } from "./BorrowedBookCard";
-import { collection, deleteDoc, doc, getDocs, setDoc } from "firebase/firestore";
+import { collection, deleteDoc, doc, getDocs, query, setDoc, where } from "firebase/firestore";
 import { auth, db } from "../../../firebase";
 
 
@@ -24,6 +24,7 @@ export const Borrow = () => {
   const [information, setInformation] = useState<any[]>([]);
   const [volumeIds, setVolumeIds] = useState<string[]>([]);
   const [displayBook, setDisplayBook] = useState(true)
+  const [sharedBook, setSharedBook] = useState();
   const handleClose = () => setOpen(false);
 
   const user = auth.currentUser;
@@ -73,9 +74,52 @@ export const Borrow = () => {
     }
     getApiData();
   }
+  const getSharedBooks = async () => {
+    const emails: string[] = [];
+    const booksList: string[] = [];
+    const volumeEmailObject: any = {}
+    const information: any = [];
+    const querySnapshot = await getDocs(collection(db, `users`));
+    querySnapshot.forEach((doc) => {
+      emails.push(doc.id);
+    });
+    // setEmails(emailList);
+    for (let i = 0; i < 8; i++) {
+      const querySnapshot2 = await getDocs(
+        // collection(db, `users/${emails[i]}/ownedBooks`)
+        query(collection(db, `users/${emails[i]}/ownedBooks`), where('isShared', '==', true))
+      );
+      querySnapshot2.forEach((doc) => {
+        information.push(doc.data());
+        booksList.push(doc.data().volumeID);
+      });
+    }
+    
+    
+    const getApiData = async () => {
+      const responseList: any = [];
+      for (let i = 0; i < 8; i++) {
+        const response = await fetch(
+
+          `https://www.googleapis.com/books/v1/volumes/${booksList[i]}`
+
+          // `https://www.googleapis.com/books/v1/volumes/${booksList[i]}?:keyes&key=AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8&maxResults=10`
+          //key1: AIzaSyB1TXi8S54_0RX2bok8fJn-OwDmBZCy6S8
+          //key2 : AIzaSyC3qM70tyz819Oy-fG929Z57AE6QtBBK3A
+          //key3: AIzaSyDxOP0RY7OpFfbHIZHByQJDGV2Hfh9rV6o
+        );
+        const data = await response.json();
+        // data.volumeInfo['info'] = 'tekst';
+        responseList.push(data.volumeInfo);
+      }
+      setSharedBook(responseList);
+    };
+    getApiData();
+  };
 
   useEffect(() => {
     getBorrowedBooks();
+    getSharedBooks();
   }, [displayBook])
 
   
@@ -123,9 +167,9 @@ export const Borrow = () => {
                   </div>
                   <div className="user-info">
                     <div className="avatar">
-                      <Avatar sx={{ bgcolor: pink[500] }}>PR</Avatar>
+                      <Avatar sx={{ bgcolor: green[500] }}>AM</Avatar>
                     </div>
-                    <div className="username">Piotr R</div>
+                    <div className="username">Anna M</div>
                   </div>
                 </div>
                 <div className="card-bottom-btn-section">
@@ -146,7 +190,7 @@ export const Borrow = () => {
               <div className="second-card-comment">
                 <div className="top">
                   <div className="rating">
-                    <Rating name="read-only" value={2.5} readOnly />
+                    <Rating name="read-only" value={3.5} readOnly />
                   </div>
                   <div className="text">
                     The elevation can be used to establish a hierarchy between
@@ -176,7 +220,7 @@ export const Borrow = () => {
               <div className="third-card-comment">
                 <div className="top">
                   <div className="rating">
-                    <Rating name="read-only" value={2.5} readOnly />
+                    <Rating name="read-only" value={5} readOnly />
                   </div>
                   <div className="text">
                     A science fiction/fantasy story about Paul Atreides, the boy
@@ -186,9 +230,9 @@ export const Borrow = () => {
                   </div>
                   <div className="user-info">
                     <div className="avatar">
-                      <Avatar sx={{ bgcolor: pink[500] }}>PR</Avatar>
+                      <Avatar sx={{ bgcolor: yellow[500] }}>MJ</Avatar>
                     </div>
-                    <div className="username">Piotr R</div>
+                    <div className="username">Marcin J</div>
                   </div>
                 </div>
                 <div className="card-bottom-btn-section">
@@ -202,17 +246,17 @@ export const Borrow = () => {
                 </div>
               </div>
               <div className="third-card-img">
-                <img src="king.jpg" alt="Shantaram" />
+                <img src="king.jpg" alt="King book picture"/>
               </div>
             </div>
             <div className="forth-card-container">
               <div className="forth-card-img">
-                <img src="jo.jpg" alt="Shantaram" />
+                <img src="jo.jpg" alt="Jo book cover" />
               </div>
               <div className="forth-card-comment">
                 <div className="top">
                   <div className="rating">
-                    <Rating name="read-only" value={2.5} readOnly />
+                    <Rating name="read-only" value={5} readOnly />
                   </div>
                   <div className="text">
                     The elevation can be used to establish a hierarchy between
@@ -222,9 +266,9 @@ export const Borrow = () => {
                   </div>
                   <div className="user-info">
                     <div className="avatar">
-                      <Avatar sx={{ bgcolor: pink[500] }}>PR</Avatar>
+                      <Avatar sx={{ bgcolor: red[500] }}>KN</Avatar>
                     </div>
-                    <div className="username">Piotr R</div>
+                    <div className="username">Karolina N</div>
                   </div>
                 </div>
                 <div className="card-bottom-btn-section">
